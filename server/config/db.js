995b9b349fe23246e305f211ded45c5bae8c1c9c
Dhanushraj07 <dhanushdharmalingam123@gmail.com>
2024-11-21 11:05:44 +0530

    db.js updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,26 @@
+// const mysql = require("mysql");
+
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "19Bec4032@",
+//     database: "user_auth"
+// });
+
+// db.connect((err) => {
+//     if (err) throw err;
+//     console.log("MySQL Connected...");
+// });
+
+// module.exports = db;
+
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "19Bec4032@",
-    database: "user_auth"
+    host: "bxv9m1gdquz8vgzl9p8h-mysql.services.clever-cloud.com",
+    user: "uweoyxmw1f7bxawj",
+    password: "9uWywuOEd0gKdaQEffPU",
+    database: "bxv9m1gdquz8vgzl9p8h"
 });
 
 db.connect((err) => {
