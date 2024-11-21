e63dd44cae53e57bdf19da09cd2702a3fa2cd314
Dhanushraj07 <dhanushdharmalingam123@gmail.com>
2024-11-21 11:44:48 +0530

    db.js updated

--- server/config/db.js
+++ server/config/db.js
@@ -14,17 +14,22 @@
 
 // module.exports = db;
 
+require("dotenv").config({ path: './server_env.env' }); // Specify the custom file path
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "bxv9m1gdquz8vgzl9p8h-mysql.services.clever-cloud.com",
-    user: "uweoyxmw1f7bxawj",
-    password: "9uWywuOEd0gKdaQEffPU",
-    database: "bxv9m1gdquz8vgzl9p8h"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306, // Default port for MySQL
 });
 
 db.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        console.error("Error connecting to MySQL:", err.message);
+        throw err;
+    }
     console.log("MySQL Connected...");
 });
 
