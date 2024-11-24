ae0db8063a2b76b67cb3466f68d0dc3a449ecd0f
Dhanushraj07 <dhanushdharmalingam123@gmail.com>
2024-11-24 09:48:55 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -12,5 +12,5 @@ app.use("/auth", authRoutes);
 // app.use('/home',(req,res)=>{
 //     res.send('<h1>Hello</h1>')
 // })
-//app.listen(5000, () => console.log("Server running on http://localhost:5000"));
-module.exports = app;
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
