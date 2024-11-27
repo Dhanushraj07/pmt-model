57869acdd183348bd0d46d519a4f47f1988fdead
Dhanushraj07 <dhanushdharmalingam123@gmail.com>
2024-11-27 12:12:21 +0530

    Home

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/auth", authRoutes);
-// app.use('/home',(req,res)=>{
-//     res.send('<h1>Hello</h1>')
-// })
+app.use('/',(req,res)=>{
+    res.send('<h1>Hello</h1>')
+})
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
