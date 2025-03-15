c4ab9c4a35ebf91097e51fa3a3d1e5eeb79741c3
Dhanushraj07 <dhanushdharmalingam123@gmail.com>
2025-03-15 10:43:25 +0530

    db connection fixed

--- server/config/db.js
+++ server/config/db.js
@@ -1,35 +1,37 @@
-const mysql = require("mysql");
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "19Bec4032@",
-    database: "user_auth"
+require('dotenv').config();
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USERNAME, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
-    if (err) throw err;
-    console.log("MySQL Connected...");
-});
+db.getConnection((err, conn) => {
+    if(err) console.log(err)
+    console.log("Connected successfully")
+})
 
 module.exports = db;
 
-// require("dotenv").config({ path: './server_env.env' }); // Specify the custom file path
+
+
 // const mysql = require("mysql");
 
 // const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT || 3306, // Default port for MySQL
+//     host: "localhost",
+//     user: "root",
+//     password: "19Bec4032@",
+//     database: "user_auth"
 // });
 
 // db.connect((err) => {
-//     if (err) {
-//         console.error("Error connecting to MySQL:", err.message);
-//         throw err;
-//     }
+//     if (err) throw err;
 //     console.log("MySQL Connected...");
 // });
 
